284c4e8e30553fc854711f367b2b4357c2cc34d8
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2023-05-13 06:03:44 -0500

    splitted methods

---
+++ readPDF.py
@@ -0,0 +1,13 @@
+from PyPDF2 import PdfReader
+
+def read_pdf(userFile):
+    """
+        Reads a PDF file and extracts its text
+        :param userFile: PDF file uploaded by user
+        :return: text extracted from PDF file
+    """
+    pdf_reader = PdfReader(userFile)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
